3d2cc1ba54af3f3c25da15c68530ba75a27eb6e3
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-16 18:56:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,17 @@ new_array = []
 def square_array(array)
   count = 0
   while count < array.length do
-    array[count] = array[count]**
+    array[count] = array[count] ** 2
     new_array.push << array[count]
     count += 1
   end 
   new_array
+end
+
+
+
+class Array
+  def square!
+    self.map! {|num| num ** 2}
+  end
 end
