e331bbc05d0d3af30bbca429f4849c7c806a3512
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-16 19:02:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   count = 0
   while count < array.length do
     array[count] ** 2
-    new_array.map(array[count])
+    new_array +=
     count += 1
   end 
   return new_array
