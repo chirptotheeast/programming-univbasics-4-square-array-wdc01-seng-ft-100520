2015cb2a1fad71a4b9109d31da302f70bb81cf23
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-16 18:45:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ new_array = []
 def square_array(array)
   count = 0
   while count < array do
-    new_array.push( array[count]) 
+    new_array.push << array[count]** 
     count += 1
   end 
   array
